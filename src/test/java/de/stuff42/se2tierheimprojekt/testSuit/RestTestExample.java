df9fa6a66a226c5703676f25a56b861beccfc48f
Veraenderer <kristian.exss@haw-hamburg.de>
2017-05-03 17:03:29 +0200

    rest test works
    random spring magic is dark

--- src/test/java/de/stuff42/se2tierheimprojekt/testSuit/RestTestExample.java
+++ src/test/java/de/stuff42/se2tierheimprojekt/testSuit/RestTestExample.java
@@ -4,65 +4,74 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import de.stuff42.se2tierheimprojekt.Application;
 import de.stuff42.se2tierheimprojekt.configuration.TestApplicationInitializer;
 import de.stuff42.se2tierheimprojekt.controller.AppStartController;
-import de.stuff42.se2tierheimprojekt.controller.test.TestController;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 
-import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.get;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-//@SpringBootTest
-@ContextConfiguration(classes = {Application.class}, initializers = {TestApplicationInitializer.class})
-
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// @SpringBootTest
+@ContextConfiguration(classes = {Application.class},
+    initializers = {TestApplicationInitializer.class})
+//@WebAppConfiguration
 public class RestTestExample {
-	    RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
-	    
-	    @Autowired
-	    AppStartController controller;
-	    
-	    @Before
-	    public void setupConnection() {
-	    	
-	    	
-	        RestAssured.baseURI = "http://localhost";
-	        RestAssured.port = 8080;
-	        RestAssured.basePath = ""; 
-	        
-	        
-	    }
-	    
-	     
-	     
-	    @Before
-	    public void setup() throws Exception {
-	        requestSpecBuilder.setContentType(ContentType.JSON).addHeader("Accept", ContentType.JSON.getAcceptHeader());
+  RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
 
-	        
-	    	//requestSpecBuilder.setContentType(ContentType.HTML).addHeader("Accept", ContentType.HTML.getAcceptHeader());
-	    }
-	    
-	    
-	    /**
-	     * Test welcher kontrolliert ob die Website ein HTML Dokument ausgibt.
-	     * @throws Exception
-	     */
-	    @Test
-	    public void beispielTest () throws Exception {
-	    	//System.out.println(get("/test").contentType().contains(ContentType.JSON.toString()));
-	    	//System.out.println(get().asString()); //nur zum experimentieren
-	    	assert(get("/test").contentType().contains(ContentType.JSON.toString()));
-	    }
-	    
-	    
-	}
+  /**
+   * 
+   */
+
+  @LocalServerPort
+  int port;
+
+  @Before
+  public void setupConnection() {
+
+
+    RestAssured.baseURI = "http://localhost";
+    RestAssured.port = port;
+    RestAssured.basePath = "";
+
+
+  }
+
+
+
+  @Before
+  public void setup() throws Exception {
+    requestSpecBuilder.setContentType(ContentType.JSON).addHeader("Accept",
+        ContentType.JSON.getAcceptHeader());
+
+
+    // requestSpecBuilder.setContentType(ContentType.HTML).addHeader("Accept",
+    // ContentType.HTML.getAcceptHeader());
+  }
+
+
+  /**
+   * Test welcher kontrolliert ob die Website ein HTML Dokument ausgibt.
+   * 
+   * @throws Exception
+   */
+  @Test
+  public void beispielTest() throws Exception {
+    // System.out.println(get("/test").contentType().contains(ContentType.JSON.toString()));
+    // System.out.println(get().asString()); //nur zum experimentieren
+    assert (get("/test").contentType().contains(ContentType.JSON.toString()));
+  }
+
+
+}
 
 
