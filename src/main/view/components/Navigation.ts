658062c667e40d4b3d008b485be6ddbd562f4d1b
Adrian Bostelmann <adrianbostelmann@googlemail.com>
2017-05-20 12:31:46 +0100

    Navigation component

---
+++ src/main/view/components/Navigation.ts
@@ -0,0 +1,17 @@
+/**
+ *  Navigation component
+ */
+export interface INavigationParams {
+
+}
+;
+
+export class Navigation {
+
+    // fetch dom element here
+    //public checkbox = $('#checkbox');
+
+    constructor(params: INavigationParams) {
+
+    }
+}
